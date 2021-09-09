35b23ddcc7de7766f2e01610c54cd34f71c37c95
letz0703 <letz0703@gmail.com>
2021-09-09 11:26:05 +0900

    splash screen
    [원노트](https://onedrive.live.com/view.aspx?resid=3A1D64E9387466DE%2113802&id=documents&wd=target%28Quiz%20Game.one%7CDB8C8A17-D749-954C-A8FC-C33E6A110072%2Fquiz%20splash%20screen%7CB3C0CF4A-2CB2-7D4D-AF21-1273FE73CD08%2F%29
    onenote:https://d.docs.live.net/3a1d64e9387466de/문서/Android/Quiz%20Game.one#quiz%20splash%20screen&section-id={DB8C8A17-D749-954C-A8FC-C33E6A110072}&page-id={B3C0CF4A-2CB2-7D4D-AF21-1273FE73CD08}&end)

--- app/src/main/java/com/letz/quizgame/Splash_Screen.java
+++ app/src/main/java/com/letz/quizgame/Splash_Screen.java
@@ -2,14 +2,44 @@ package com.letz.quizgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash_Screen extends AppCompatActivity
 {
+
+    ImageView imageView;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        imageView = findViewById(R.id.ivSplash);
+        title = findViewById(R.id.tvSplash);
+
+        Animation animation = AnimationUtils.loadAnimation(
+                getApplicationContext()
+                , R.anim.ani_splash
+        );
+
+        title.startAnimation(animation);
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash_Screen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
     }
 }
