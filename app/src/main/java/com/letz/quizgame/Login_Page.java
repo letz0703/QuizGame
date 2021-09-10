e53c00df4c57c09d9b36212b3b9f548bd78f400f
letz0703 <letz0703@gmail.com>
2021-09-10 11:14:54 +0900

    Login 화면 디자인 w/ 구글 로그인

---
+++ app/src/main/java/com/letz/quizgame/Login_Page.java
@@ -0,0 +1,15 @@
+package com.letz.quizgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Page extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
