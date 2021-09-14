bfb223d9895f9237c85076cf73b5f1b440006d65
letz0703 <letz0703@gmail.com>
2021-09-14 10:34:11 +0900

    main : signOut
    	FirebaseAuth 생성
    	auth.signOut()
    	Login_Page로 돌아가기

--- app/src/main/java/com/letz/quizgame/MainActivity.java
+++ app/src/main/java/com/letz/quizgame/MainActivity.java
@@ -2,14 +2,29 @@ package com.letz.quizgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity
 {
+    TextView signOut;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signOut = findViewById(R.id.tvSignOut);
+
+        signOut.setOnClickListener(v -> {
+            auth.signOut();
+            Intent igoLogin_Page = new Intent(MainActivity.this, Login_Page.class);
+            startActivity(igoLogin_Page);
+            finish();
+        });
     }
 }
