d88760c2d6eadfad9e1783741cdb577eeebbadf9
letz <letz0703@gmail.com>
2021-09-23 21:06:37 +0900

    wip home

--- app/src/main/java/com/letz/quizgame/QuizPage.java
+++ app/src/main/java/com/letz/quizgame/QuizPage.java
@@ -28,11 +28,14 @@ public class QuizPage extends AppCompatActivity
     String quizAnswerB;
     String quizAnswerC;
     String quizAnswerD;
-    String tvCorrectAnswer;
+    String dbAnswer;
     int questionCount;
     int questionNumber = 1;
 
+
     String userAnswer;
+    int countUserCorrect = 0;
+    int countUserWrong = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,43 +54,88 @@ public class QuizPage extends AppCompatActivity
         finish = findViewById(R.id.btnFinish_QuizPage);
 
         game();
-        
+
         next.setOnClickListener(v -> {
             game();
+            a.setBackgroundColor(Color.WHITE);
+            b.setBackgroundColor(Color.WHITE);
+            c.setBackgroundColor(Color.WHITE);
+            d.setBackgroundColor(Color.WHITE);
         });
-        
+
         finish.setOnClickListener(v -> {
-            
+
         });
 
         a.setOnClickListener(v -> {
             userAnswer = "a";
-            if (tvCorrectAnswer.equals(userAnswer))
-            {
+            if (dbAnswer.equals(userAnswer)) {
                 a.setBackgroundColor(Color.GREEN);
+                countUserCorrect++;
+                correct.setText("" + countUserCorrect);
 
             } else {
-
-            };
+                a.setBackgroundColor(Color.RED);
+                Toast.makeText(QuizPage.this, "Wrong!!!", Toast.LENGTH_SHORT).show();
+                countUserWrong++;
+                wrong.setText("" + countUserWrong);
+                markCorrectAnswer();
+            }
         });
         b.setOnClickListener(v -> {
             userAnswer = "b";
+            if (dbAnswer.equals(userAnswer)) {
+                b.setBackgroundColor(Color.GREEN);
+                countUserCorrect++;
+                correct.setText("" + countUserCorrect);
+
+            } else {
+                b.setBackgroundColor(Color.RED);
+                Toast.makeText(QuizPage.this, "Wrong!!!", Toast.LENGTH_SHORT).show();
+                countUserWrong++;
+                wrong.setText("" + countUserWrong);
+                markCorrectAnswer();
+            }
+            ;
         });
         c.setOnClickListener(v -> {
             userAnswer = "c";
+            if (dbAnswer.equals(userAnswer)) {
+                c.setBackgroundColor(Color.GREEN);
+                countUserCorrect++;
+                correct.setText("" + countUserCorrect);
+
+            } else {
+                c.setBackgroundColor(Color.RED);
+                Toast.makeText(QuizPage.this, "Wrong!!!", Toast.LENGTH_SHORT).show();
+                countUserWrong++;
+                wrong.setText("" + countUserWrong);
+                markCorrectAnswer();
+            }
+            ;
         });
         d.setOnClickListener(v -> {
             userAnswer = "d";
+            if (dbAnswer.equals(userAnswer)) {
+                d.setBackgroundColor(Color.GREEN);
+                countUserCorrect++;
+                correct.setText("" + countUserCorrect);
+
+            } else {
+                d.setBackgroundColor(Color.RED);
+                Toast.makeText(QuizPage.this, "Wrong!!!", Toast.LENGTH_SHORT).show();
+                countUserWrong++;
+                wrong.setText("" + countUserWrong);
+                markCorrectAnswer();
+            }
+            ;
         });
-
-
-        
-
     }
-    public void game()
-    {
+
+    public void game() {
         // Read from the database
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        databaseReference.addValueEventListener(new ValueEventListener()
+        {
 
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -95,27 +143,24 @@ public class QuizPage extends AppCompatActivity
                 // whenever data at this location is updated.
 //                String value = dataSnapshot.getValue(String.class);
                 questionCount = (int) dataSnapshot.getChildrenCount();
-                
+
                 quizQuestion = dataSnapshot.child(String.valueOf(questionNumber)).child("q").getValue().toString();
                 quizAnswerA = dataSnapshot.child(String.valueOf(questionNumber)).child("a").getValue().toString();
                 quizAnswerB = dataSnapshot.child(String.valueOf(questionNumber)).child("b").getValue().toString();
-                quizAnswerC= dataSnapshot.child(String.valueOf(questionNumber)).child("c").getValue().toString();
+                quizAnswerC = dataSnapshot.child(String.valueOf(questionNumber)).child("c").getValue().toString();
                 quizAnswerD = dataSnapshot.child(String.valueOf(questionNumber)).child("d").getValue().toString();
-                tvCorrectAnswer = dataSnapshot.child(String.valueOf(questionNumber)).child("answer").getValue().toString();
+                dbAnswer = dataSnapshot.child(String.valueOf(questionNumber)).child("answer").getValue().toString();
 
                 question.setText(quizQuestion);
                 a.setText(quizAnswerA);
                 b.setText(quizAnswerB);
                 c.setText(quizAnswerC);
                 d.setText(quizAnswerD);
-                
-                if (questionNumber < questionCount)
-                {
+
+                if (questionNumber < questionCount) {
                     questionNumber++;
-                }
-                else
-                {
-                    Toast.makeText(QuizPage.this, "You answered all questions", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(QuizPage.this, "Finished", Toast.LENGTH_SHORT).show();
                 }
             }
 
@@ -126,4 +171,17 @@ public class QuizPage extends AppCompatActivity
             }
         });
     }
+
+    public void markCorrectAnswer() {
+        if (dbAnswer.equals("a")) {
+            a.setBackgroundColor(Color.GREEN);
+        } else if (dbAnswer.equals("b")) {
+            b.setBackgroundColor(Color.GREEN);
+        } else if (dbAnswer.equals("c")) {
+            c.setBackgroundColor(Color.GREEN);
+        } else if (dbAnswer.equals("d")) {
+            d.setBackgroundColor(Color.GREEN);
+        }
+    }
+
 }
